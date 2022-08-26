50b1b7520c00664f0a0edd79c5982cdaa105f234
Hastings <jw.hastings@wsu.edu>
2022-08-26 11:48:07 -0700

    Add both startup paths

--- src/wrapper_startup.m
+++ src/wrapper_startup.m
@@ -2,8 +2,9 @@
 
 %% add MATPOWER paths
 
-MATPOWER_start_up_path = '/home/helics-user/Projects/HELICS_Plus/matpower7.1/matpower_startup.m';
-run(MATPOWER_start_up_path)
+% MATPOWER_start_up_path = '/home/helics-user/Projects/HELICS_Plus/matpower7.1/matpower_startup.m';
+% MATPOWER_start_up_path = 'C:\Users\jw.hastings\Documents\matpower7.1\startup.m';
+% run(MATPOWER_start_up_path)
 
 %% add HELICS paths
 % addpath('/home/monish/Projects/Softwares/Helics_install/octave');
